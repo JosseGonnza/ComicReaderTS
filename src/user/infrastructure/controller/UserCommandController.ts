46766673b4c6bfaee5893d0d547f74fae8169804
JosseGonnza <accion.quevedo@hotmail.com>
2024-10-01 21:13:52 +0100

    ✨ feat: create UserCommandController.ts

--- src/user/infrastructure/controller/UserCommandController.ts
+++ src/user/infrastructure/controller/UserCommandController.ts
@@ -1,2 +1,43 @@
-export class UserCommandController{
+import { Request, Response } from 'express';
+import {DeleteUser, DeleteUserRequest} from "../../application/useCases/DeleteUser";
+import {UpdateUser, UpdateUserRequest} from "../../application/useCases/UpdateUser";
+import {FakeRepository} from "../repositories/FakeRepository";
+import {CreateUser, CreateUserRequest} from "../../application/useCases/CreateUser";
+
+export class UserCommandController {
+    private createUser: CreateUser;
+    private updateUser: UpdateUser;
+    private deleteUser: DeleteUser;
+
+    constructor(repository: FakeRepository) {
+        this.createUser = new CreateUser(repository);
+        this.updateUser = new UpdateUser(repository);
+        this.deleteUser = new DeleteUser(repository);
+    }
+
+    post(req: Request, res: Response): void {
+        const request: CreateUserRequest = req.body;
+        const user = this.createUser.execute(request);
+        res.status(201).json({ message: "User created", user: user });
+    }
+
+    put(req: Request, res: Response): void {
+        const request: UpdateUserRequest = req.body;
+        const updatedUser = this.updateUser.execute(request);
+        if (updatedUser) {
+            res.status(200).json({ message: "User updated", updatedUser });
+        } else {
+            res.status(404).send("User not found");
+        }
+    }
+
+    delete(req: Request, res: Response): void {
+        const request: DeleteUserRequest = { id: req.params.id };
+        const deleted = this.deleteUser.execute(request);
+        if (deleted) {
+            res.status(204).send("User deleted");
+        } else {
+            res.status(404).send("User not found");
+        }
+    }
 }
