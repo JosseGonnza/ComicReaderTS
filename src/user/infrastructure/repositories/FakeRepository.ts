5376f694a5e473532974323c882fb9f5329e99d4
JosseGonnza <accion.quevedo@hotmail.com>
2024-10-01 21:12:00 +0100

    ✨ feat: create FakeRepository.ts and implements two interfaces

---
+++ src/user/infrastructure/repositories/FakeRepository.ts
@@ -0,0 +1,47 @@
+import {IUserCommandRepository} from "../../domain/ports/IUserCommandRepository";
+import {User} from "../../domain/entities/User";
+import {v4 as uuidv4} from "uuid"
+import {IUserQueryRepository} from "../../domain/ports/IUserQueryRepository";
+
+export class FakeRepository implements IUserCommandRepository, IUserQueryRepository{
+    private users: User[] = [];
+
+    getAll(): User[] {
+        return this.users;
+    }
+
+    getById(id: string): User | undefined {
+        return this.users.find(user => user.id === id);
+    }
+
+    save(user: User): User {
+        const existingUserIndex = this.users.findIndex(u => u.id === user.id);
+
+        if (existingUserIndex !== -1) {
+            this.users[existingUserIndex] = { ...this.users[existingUserIndex], ...user };
+        } else {
+            user.id = uuidv4();
+            this.users.push(user);
+        }
+        return user;
+    }
+
+    update(id: string, userData: Partial<User>): User | undefined {
+        const user = this.getById(id);
+        if (user) {
+            Object.assign(user, userData);
+            this.save(user);
+            return user;
+        }
+        return undefined;
+    }
+
+    delete(id: string): boolean {
+        const index = this.users.findIndex(user => user.id === id);
+        if (index !== -1) {
+            this.users.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+}
