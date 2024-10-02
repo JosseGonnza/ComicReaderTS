9ff641977c95c5799ecab141503f022fac305466
JosseGonnza <accion.quevedo@hotmail.com>
2024-10-02 09:18:46 +0100

    ✨ feat: implement comic controllers

---
+++ src/comic/infrastructure/controllers/ComicCommandController.ts
@@ -0,0 +1,35 @@
+import {ComicRepository} from "../repositories/ComicRepository";
+import {Comic} from "../../domain/entities/Comic";
+import { Request, Response } from "express";
+
+export class ComicCommandController{
+    private comicRepository: ComicRepository;
+
+    constructor(comicRepository: ComicRepository) {
+        this.comicRepository = comicRepository;
+    }
+
+    createComic(req: Request, res: Response): void {
+        const { name, author, tomos } = req.body;
+        const userId = req.params.userId; // ID del usuario al que se le asignará el cómic
+
+        const newComic = new Comic(name, author, tomos);
+        const savedComic = this.comicRepository.save(newComic, userId);
+
+        res.status(201).json({
+            message: "Comic created and assigned to user",
+            comic: savedComic
+        });
+    }
+
+    deleteComic(req: Request, res: Response): void {
+        const comicId = req.params.id;
+        const deleted = this.comicRepository.delete(comicId);
+
+        if (!deleted) {
+            res.status(404).json({ message: "Comic not found" });
+        } else {
+            res.status(200).json({ message: "Comic deleted" });
+        }
+    }
+}
---
+++ src/comic/infrastructure/controllers/ComicQueryController.ts
@@ -0,0 +1,38 @@
+import {ComicRepository} from "../repositories/ComicRepository";
+import { Request, Response } from "express";
+
+
+export class ComicQueryController {
+    private comicRepository: ComicRepository;
+
+    constructor(comicRepository: ComicRepository) {
+        this.comicRepository = comicRepository;
+    }
+
+    getAllComics(req: Request, res: Response): void {
+        const comics = this.comicRepository.getAll();
+        res.status(200).json(comics);
+    }
+
+    getComicById(req: Request, res: Response): void {
+        const comicId = req.params.id;
+        const comic = this.comicRepository.getById(comicId);
+
+        if (!comic) {
+            res.status(404).json({ message: "Comic not found" });
+        } else {
+            res.status(200).json(comic);
+        }
+    }
+
+    getComicsByUser(req: Request, res: Response): void {
+        const userId = req.params.userId;
+        const comics = this.comicRepository.getComicsByUser(userId);
+
+        if (comics.length === 0) {
+            res.status(404).json({ message: "No comics found for this user" });
+        } else {
+            res.status(200).json(comics);
+        }
+    }
+}
