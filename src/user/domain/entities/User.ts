d43efbb6ed9608c3195cb7e74cbd4fe412756725
JosseGonnza <accion.quevedo@hotmail.com>
2024-10-02 08:17:27 +0100

    ♻️ refactor: User class

--- src/user/domain/entities/User.ts
+++ src/user/domain/entities/User.ts
@@ -1,3 +1,23 @@
+import {UserComic} from "../../../userComic/domain/entities/UserComic";
+
+export class User {
+    id: string;
+    nickName: string;
+    email: string;
+    password: string;
+    phone: string;
+    userComics: UserComic[]; // Relación con tabla intermedia
+
+    constructor(nickName: string, email: string, password: string, phone: string) {
+        this.id = ''; // ID se genera al guardar un user
+        this.nickName = nickName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userComics = []; // Relación vacía al crearse el user
+    }
+}
+
 export interface User {
     id: string;
     nickName: string;
