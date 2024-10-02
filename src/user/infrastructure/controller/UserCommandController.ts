37f29bff1847c098c672af55313860ec0ce7409e
JosseGonnza <accion.quevedo@hotmail.com>
2024-10-02 08:06:08 +0100

    🩹 fix: method update is ready

--- src/user/infrastructure/controller/UserCommandController.ts
+++ src/user/infrastructure/controller/UserCommandController.ts
@@ -22,10 +22,13 @@ export class UserCommandController {
     }
 
     put(req: Request, res: Response): void {
+        const { id } = req.params;  // Obtener el ID de la URL
         const request: UpdateUserRequest = req.body;
-        const updatedUser = this.updateUser.execute(request);
-        if (updatedUser) {
-            res.status(200).json({ message: "User updated", updatedUser });
+        request.id = id;  // Asegurar que el ID esté en el request
+
+        const user = this.updateUser.execute(request);
+        if (user) {
+            res.status(200).json({ message: "User updated", updatedUser: user });
         } else {
             res.status(404).send("User not found");
         }
